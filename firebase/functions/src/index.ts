2410eafc493c4ee0b815219c2771c53ecc21adb5
dsluo <me@dsluo.dev>
2019-11-25 22:50:25 -0500

    use stderr for errors and catch errors on delete.

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -7,12 +7,17 @@ export const createUser = functions.auth.user().onCreate(user => {
     const userDoc = {
         groups: []
     };
-    return admin.firestore().collection('users').doc(user.uid).set(userDoc)
-        .catch(reason => {
-            console.log('Failed to create user document:', reason);
-        });
+    return admin.firestore()
+        .collection('users')
+        .doc(user.uid)
+        .set(userDoc)
+        .catch(reason => console.error('Failed to create user document.', reason));
 });
 
 export const deleteUser = functions.auth.user().onDelete((user, _context) => {
-    return admin.firestore().collection('users').doc(user.uid).delete();
+    return admin.firestore()
+        .collection('users')
+        .doc(user.uid)
+        .delete()
+        .catch(reason => console.error('Failed to delete user document.', reason));
 });
